d5fb89811560ee7079fbcac00603379dfc41d1a1
Rahul Tripathi <tripathirahul796@gmail.com>
2025-12-07 23:35:06 +0530

    Added request mapping to QRCodeGeneratorController

--- src/main/java/com/onlineordersystem/onlineordersystem/controller/QRCodeGeneratorController.java
+++ src/main/java/com/onlineordersystem/onlineordersystem/controller/QRCodeGeneratorController.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.google.zxing.WriterException;
 import com.onlineordersystem.onlineordersystem.service.QRCode.QRCodeGenerator;
 
 @RestController
+@RequestMapping("/qr")
 public class QRCodeGeneratorController {
 
     @Autowired
